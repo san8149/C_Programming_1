ae3f015c420c57aa158a49710f91f3bea45a46c8
san8149 <san8149@naver.com>
2021-09-21 01:24:35 +0900

    hw1.c

--- hw1.c
+++ hw1.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-void main()
+int main(void)
 {
-	printf("C프로그래밍 1주차 과제\n");	
-	printf("이름 : 이창진\n");
-	printf("학번 : %d\n", 201810275);
+	int num1;
+	int num2;
+	printf("두 정수를 입력하세요:");
+	scanf("%d %d", &num1, &num2);	
+	printf("%d & %d = %d", num1, num2, num1 & num2);
+	printf("\n%d | %d = %d", num1, num2, num1 | num2);
+	printf("\n%d ^ %d = %d", num1, num2, num1 ^ num2);
 	return 0;
 }
